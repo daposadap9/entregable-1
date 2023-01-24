3a4a5a7b329d4b55e04a744d12592fae48d59bf0
Diego Alejandro Posada <diegoposada1999@gmail.com>
2023-01-23 23:00:03 -0500

    Se agrega animacion

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import QuoteBox from './components/QuoteBox'
 import db from "./db/quotes.json"
-
+import { motion } from 'framer-motion';
 const arrayColors = ["#00c6ab","#f86f6f","#6aa3b4","#A4303F",]
   const getRandom = (arrayElements) => {
     const quantityValues = arrayElements.length
@@ -19,12 +19,16 @@ const [color, setColor]=useState(getRandom(arrayColors))
   }
 
   return (
-    <div className="App" style={{backgroundColor: color}}>
+    <motion.div 
+    initial={{width: 0}}
+    animate={{width: "100%"}}
+    exit={{x:window.innerWidth, transition:{duration: 0.1}}}
+    className="App" style={{backgroundColor: color}}>
       <QuoteBox 
       quote={quote} 
       newQuote={newQuote}
       color={color}/>
-    </div>
+    </motion.div>
   )
 }
 
--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,10 +1,10 @@
 import React from "react"
 import QuoteButton from "./QuoteButton"
-
 const QuoteBox = ({quote, newQuote, color})  =>{
 
 return (
-    <div className="quoteBox" style={{color:color}}>
+    <div
+    className="quoteBox" style={{color:color}}>
         <p className="quoteBox__quote">{quote.quote}</p>
         <h3 className="quoteBox__author">{quote.author}</h3>
         <i className="quoteBox__icon bx bxs-quote-left"></i>
